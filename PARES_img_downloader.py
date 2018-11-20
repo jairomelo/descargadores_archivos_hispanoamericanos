8a132d62f3060c6b0eef98a3b124f1943193e18d
jairomelo <jairom@colmich.edu.mx>
2018-11-19 22:02:09 -0600

    opción de descarga - still not working :'(

--- PARES_img_downloader.py
+++ PARES_img_downloader.py
@@ -1,6 +1,6 @@
 import urllib.request
 import re
-import glob, wget, csv
+import glob, wget, csv, os
 
 print("Antes de utilizar este archivo, asegúrese que la carpeta desde la cual se ejecuta esté vacía de archivos *.jpg y *.txt")
 wait = input("Presione ENTER para continuar.")
@@ -9,6 +9,7 @@ wait = input("Presione ENTER para continuar.")
 
 csv_file = input('Ingresa el nombre de tu archivo *.csv: ')
 txt_file = input('Nombre del archivo *.txt de salida: ')
+directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')
 with open(txt_file, "w") as my_output_file:
     with open(csv_file, "r") as my_input_file:
         [ my_output_file.write(", ".join(row)) for row in csv.reader(my_input_file, delimiter=' ', quotechar='|')]
@@ -36,28 +37,8 @@ repeticion = mi_cadena
 rago = len(repeticion) -1
 
 print("se están descargando los archivos")
-
+os.makedirs(directorio)
 # loop para llamar cada página del repositorio y recolectar solo la información de los div que contengan la clase 'element-text'
 for i in range(rago):
-        ## Prueba 1
-        url = url_base.format(repeticion[i])
-        req = urllib.request.Request(url, method='HEAD')
-        r = urllib.request.urlopen(req)
-        print(r.info().get_filename())
-        
-        ## Prueba 2
-        #try: 
-                #wget.download(url_base.format(repeticion[i]))
-        #else:
-                #print("algo salió mal :'(")
-        
-        ## Prueba 3        
-        #urllib.request.urlretrieve(url_base.format(repeticion[i]))
-        #r = requests.get(url_base.format(repeticion[i]))
-        #d = r.headers('content-disposition')
-        #fname = re.findall("filename=(.+)", d)
-
-## Parte de prueba 3
-#print(r.status_code)  
-#print(r.headers['content-type'])  
-#print(r.encoding)  
+    url = url_base.format(repeticion[i])
+    down = urllib.request.urlretrieve(url, "%s/%s.jpg" % (directorio, i))

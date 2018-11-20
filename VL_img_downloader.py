6cf51e98a389cd8057df49f655f6001e7ba55850
jairomelo <jairom@colmich.edu.mx>
2018-11-19 20:39:40 -0600

    mejorado el input del usuario

--- VL_img_downloader.py
+++ VL_img_downloader.py
@@ -1,6 +1,7 @@
 import urllib.request
 import wget, os
 
+enlace = input('Enlace del número del periódico. <br /> Pegue la liga solamente hasta el signo de % \(ejemplo: http://hemeroteca.vanguardia.com/hemeroteca/public/periodico/img_pdf/vl/1953/1953-11-02/vl19531102%\)<br /> : ')
 directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')
 init = input('Número de la imágen inicial: ')
 fin = input('Número de la página final: ')
@@ -10,7 +11,7 @@ os.makedirs(directorio)
 init_i = int(init)
 fin_i = int(fin)
 
-url_base = "http://hemeroteca.vanguardia.com/hemeroteca/public/periodico/img_pdf/vl/1953/1953-11-02/vl19531102%{}.jpg"
+url_base = "%s{}.jpg" % enlace
 lista = range(init_i,fin_i)
 
 rango = len(lista)

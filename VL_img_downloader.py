a939b1de175e1bf629c35095d9985d894e36d3fb
jairomelo <jairom@colmich.edu.mx>
2018-11-19 20:31:19 -0600

    agregados inputs de usuario

--- VL_img_downloader.py
+++ VL_img_downloader.py
@@ -1,10 +1,20 @@
 import urllib.request
-import wget
+import wget, os
+
+directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')
+init = input('Número de la imágen inicial: ')
+fin = input('Número de la página final: ')
+
+os.makedirs(directorio)
+
+init_i = int(init)
+fin_i = int(fin)
 
 url_base = "http://hemeroteca.vanguardia.com/hemeroteca/public/periodico/img_pdf/vl/1953/1953-11-02/vl19531102%{}.jpg"
-lista = "201,202,203,204,205,206,207,208,209"
-repeticion = lista.split(",")
+lista = range(init_i,fin_i)
 
-for i in range(0,8):
-    url = url_base.format(repeticion[i])
-    down = urllib.request.urlretrieve(url, "%s.jpg" % (i))
+rango = len(lista)
+
+for i in range(rango):
+    url = url_base.format(lista[i])
+    down = urllib.request.urlretrieve(url, "%s/%s.jpg" % (directorio, i))

96eb0464e4b776c2f7f61de1e03d55a81a241f65
jairomelo <jairom@colmich.edu.mx>
2018-11-19 21:47:25 -0600

    eliminado módulo sin uso

--- VL_img_downloader.py
+++ VL_img_downloader.py
@@ -1,5 +1,5 @@
 import urllib.request
-import wget, os
+import os
 
 enlace = input('Enlace del número del periódico. <br /> Pegue la liga solamente hasta el signo de % \(ejemplo: http://hemeroteca.vanguardia.com/hemeroteca/public/periodico/img_pdf/vl/1953/1953-11-02/vl19531102%\)<br /> : ')
 directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')

85ea4e13a58f6ed5008248f6fc1eb73fbb3223e9
jairomelo <jairom@colmich.edu.mx>
2018-11-19 22:14:56 -0600

    opción directorio. Evita error por múltiples archivos txt

--- AGNC_img_downloader.py
+++ AGNC_img_downloader.py
@@ -1,5 +1,5 @@
 import urllib.request
-import glob, wget, csv
+import glob, wget, csv, os
 
 print("Antes de utilizar este archivo, asegúrese que la carpeta desde la cual se ejecuta esté vacía de archivos *.jpg y *.txt")
 wait = input("Presione ENTER para continuar.")
@@ -8,6 +8,7 @@ wait = input("Presione ENTER para continuar.")
 
 csv_file = input('Ingresa el nombre de tu archivo *.csv: ')
 txt_file = input('Nombre del archivo *.txt de salida: ')
+directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')
 with open(txt_file, "w") as my_output_file:
     with open(csv_file, "r") as my_input_file:
         [ my_output_file.write(", ".join(row)) for row in csv.reader(my_input_file, delimiter=' ', quotechar='|')]
@@ -15,7 +16,7 @@ with open(txt_file, "w") as my_output_file:
 
 # extraer el nombre del archivo de texto
 
-txtnomb = glob.glob("*.txt")
+txtnomb = glob.glob("%s" % (txt_file))
 cadena = str(txtnomb)
 encadenado = ''.join(cadena).replace('[\'','').replace('\']','').replace(' ','')
 
@@ -33,11 +34,11 @@ repeticion = mi_cadena
 rago = len(repeticion) -1
 
 print("se están descargando %d imágenes" % (rago))
-
+os.makedirs(directorio)
 # loop para descargar las imágenes o mostrar un mensaje de error
 for i in range(rago):
         try: 
-                wget.download(url_base.format(repeticion[i]), "%s.jpg" % (i))
+                wget.download(url_base.format(repeticion[i]), "%s/%s.jpg" % (directorio, i))
         except:
                 sys.exit("algo salió mal :(")
 

e66e43a1b5c955c7993b6feef0584cea6fabd9ba
jairomelo <jairom@colmich.edu.mx>
2018-11-22 03:21:51 -0600

    PARES funcional pero no ideal

--- PARES_img_downloader.py
+++ PARES_img_downloader.py
@@ -1,44 +1,27 @@
-import urllib.request
-import re
-import glob, wget, csv, os
+import requests, os, time
 
-print("Antes de utilizar este archivo, asegúrese que la carpeta desde la cual se ejecuta esté vacía de archivos *.jpg y *.txt")
-wait = input("Presione ENTER para continuar.")
-
-#Convierte archivo csv a texto
-
-csv_file = input('Ingresa el nombre de tu archivo *.csv: ')
-txt_file = input('Nombre del archivo *.txt de salida: ')
 directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')
-with open(txt_file, "w") as my_output_file:
-    with open(csv_file, "r") as my_input_file:
-        [ my_output_file.write(", ".join(row)) for row in csv.reader(my_input_file, delimiter=' ', quotechar='|')]
-    my_output_file.close()
+ident = input('Número de la colección: ')
+init = input('Número de la imagen inicial: ')
+fin = input('Número de la página final: ')
 
-# extraer el nombre del archivo de texto
+url_entrada = 'http://pares.mcu.es/ParesBusquedas20/catalogo/show/{}'.format(ident)
+url_imagen = 'http://pares.mcu.es/ParesBusquedas20/ViewImage.do?accion=42&txt_id_imagen=1&txt_rotar=0&txt_contraste=0&dbCode={}'
 
-txtnomb = glob.glob("*.txt")
-cadena = str(txtnomb)
-encadenado = ''.join(cadena).replace('[\'','').replace('\']','').replace(' ','')
+s = requests.Session()
+# La primera petición obtiene las cookies [https://es.stackoverflow.com/a/215235/51760]
+s.get(url_entrada)
 
-
-# convierte el texto en un array
-
-mi_lista = open(encadenado, 'r')
-mi_listado = mi_lista.read()
-mi_cadena = mi_listado.split(",")
-
-#################
-
-# guarda en la función "url_base" la dirección "estática" de cada elemento
-url_base = "http://pares.mcu.es/ParesBusquedas20/ViewImage.do?accion=42&txt_id_imagen={}"
-repeticion = mi_cadena
-
-rago = len(repeticion) -1
-
-print("se están descargando los archivos")
 os.makedirs(directorio)
-# loop para llamar cada página del repositorio y recolectar solo la información de los div que contengan la clase 'element-text'
-for i in range(rago):
-    url = url_base.format(repeticion[i])
-    down = urllib.request.urlretrieve(url, "%s/%s.jpg" % (directorio, i))
+
+init_i = int(init)
+fin_i = int(fin)
+
+lista = range(init_i,fin_i)
+
+for i in range(len(lista)):
+	url = url_imagen.format(lista[i])
+	r = s.get(url)
+	with open("%s/%s.jpg" % (directorio, i), 'wb') as handler:
+		handler.write(r.content)
+	time.sleep(3)

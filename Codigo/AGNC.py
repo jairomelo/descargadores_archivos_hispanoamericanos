560db82f9c7f409141cb6cedf3c057972d3f71a7
jairomelo <jairom@colmich.edu.mx>
2019-07-24 22:07:25 -0500

    muestra errores HTTP, indica el # de la img y el enlace que no fue posible descargar, y continúa con el loop

--- Codigo/AGNC.py
+++ Codigo/AGNC.py
@@ -1,5 +1,6 @@
+import os
+import time
 import urllib.request
-import os, time
 
 enlace = "http://consulta.archivogeneral.gov.co/ConsultaWeb/assets/image?id="
 directorio = input('Ingresa el nombre de la carpeta para guardar las imágenes: ')
@@ -9,21 +10,21 @@ fin = input('Número de la página final: ')
 init_i = int(init)
 fin_i = int(fin) + 1
 
-lista = range(init_i,fin_i)
+lista = range(init_i, fin_i)
 
 if not os.path.exists(directorio):
-	os.makedirs(directorio)
+    os.makedirs(directorio)
 
-print("Se van a descargar {} imágenes".format(fin_i-init_i))
+print("Se van a descargar {} imágenes".format(fin_i - init_i))
 
 for i in range(len(lista)):
-        url = "{}{}".format(enlace, lista[i])
-        print("descargando img {}".format(i+1))
-        if not os.path.exists("{}/{}.jpg".format(directorio, i+1)):
-                urllib.request.urlretrieve(url, "{}/{}.jpg".format(directorio, i+1))
-                time.sleep(2)
+    url = "{}{}".format(enlace, lista[i])
+    print("\r" + "Descargando img {}".format(i + 1) + ".", end="")
+    try:
+        if not os.path.exists("{}/{}.jpg".format(directorio, i + 1)):
+            urllib.request.urlretrieve(url, "{}/{}.jpg".format(directorio, i + 1))
+            time.sleep(3)
         else:
-                print("la imagen ya existe")
-                pass
-            
-
+            pass
+    except urllib.error.HTTPError as err:
+        print("No se pudo descargar la img {} del enlace {}".format(i + 1, url) + "\n" + "ERROR {}".format(err.code))

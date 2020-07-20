6db14874147b14722a4ea2626cfe42c3e7a9e609
jairomelo <jairom@colmich.edu.mx>
2020-07-19 19:24:11 -0500

    arreglo en leg. <10 docs y en nombre archivo

--- Codigo/test_AGNMX.py
+++ Codigo/test_AGNMX.py
@@ -1,5 +1,9 @@
 from AGNMX import imagenes
 
+'''
+Para descargar el legajo solamente cambie la url por la que desee descargar.
+'''
+
 url = "https://archivos.gob.mx/guiageneral/visorimg/visorimg.php?CodR=MX09017AGNCL01FO001AYSE001APUI001"
 
 try:
